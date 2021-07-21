efd9293e216cb462d25036c55aeb883d0a4e6e04
Steve Clark <steve.a.clark48@gmail.com>
2021-07-21 14:54:52 -0500

    added inventory and register

--- simulation.py
+++ simulation.py
@@ -6,22 +6,20 @@ class Simulation:
 
 
     def __init__(self):
-        
+        pass
 
-        def run_simulation(self):
-            """The central method called in main.py."""
-            customer = Customer()
-            soda_machine = SodaMachine()
-            soda_machine.fill_inventory()
-            soda_machine.fill_register()
-            will_proceed = True
-            while will_proceed:
-                user_option = user_interface.simulation_main_menu()
-                if user_option == "1":
-                    soda_machine.begin_transaction(customer)
-                elif user_option == "2":
-                    customer.check_coins_in_wallet()
-                elif user_option == "3":
-                    customer.check_backpack()
-                else:
-                    will_proceed = False
+    def run_simulation(self):
+        """The central method called in main.py."""
+        customer = Customer()
+        soda_machine = SodaMachine()
+        will_proceed = False
+        while will_proceed == False:
+            user_option = user_interface.simulation_main_menu()
+            if user_option == "1":
+                soda_machine.begin_transaction(customer)
+            elif user_option == "2":
+                customer.check_coins_in_wallet()
+            elif user_option == "3":
+                customer.check_backpack()
+            else:
+                will_proceed = False
