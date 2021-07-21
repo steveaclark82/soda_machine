0519ad65ae25fc23c872aa305d089c37c47a257d
Steve Clark <steve.a.clark48@gmail.com>
2021-07-21 14:51:04 -0500

    added soda inventory and register

--- simulation.py
+++ simulation.py
@@ -12,7 +12,9 @@ class Simulation:
             """The central method called in main.py."""
             customer = Customer()
             soda_machine = SodaMachine()
-            will_proceed = False
+            soda_machine.fill_inventory()
+            soda_machine.fill_register()
+            will_proceed = True
             while will_proceed:
                 user_option = user_interface.simulation_main_menu()
                 if user_option == "1":
