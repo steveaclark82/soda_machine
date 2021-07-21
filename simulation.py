127e1edf84925e6fa7cd00764cc4bb036f57f479
TBuscemi <thomasbuscemi124@gmail.com>
2021-07-21 16:44:35 -0400

    up dated the int in run_simulation. add user_input to simulation_main_menu. added name to select_soda on line 45 in soda_machine class

--- simulation.py
+++ simulation.py
@@ -15,12 +15,12 @@ class Simulation:
         soda_machine = SodaMachine()
         will_proceed = False
         while will_proceed == False:
-            user_option = user_interface.simulation_main_menu()
-            if user_option == "1":
+            user_option = user_interface.simulation_main_menu() 
+            if user_option == 1:
                 soda_machine.begin_transaction(customer)
-            elif user_option == "2":
+            elif user_option == 2:
                 customer.check_coins_in_wallet()
-            elif user_option == "3":
+            elif user_option == 3:
                 customer.check_backpack()
             else:
                 will_proceed = False
