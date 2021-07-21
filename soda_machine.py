6eb9142ade3f659d5ea46f4ffaa0648fa3d77d7b
Steve Clark <steve.a.clark48@gmail.com>
2021-07-21 15:31:15 -0500

    changed greater than less than sign on line 55

--- soda_machine.py
+++ soda_machine.py
@@ -34,7 +34,7 @@ class SodaMachine:
 
     def begin_transaction(self, customer):
         """Method is complete. Initiates purchase if user decides to proceed. No errors."""
-        will_proceed = self.user_interface.display_welcome()
+        will_proceed = user_interface.display_welcome()
         if will_proceed:
             self.run_transaction(customer)
 
@@ -52,11 +52,11 @@ class SodaMachine:
 
     def calculate_transaction(self, customer_payment, selected_soda, customer):
         total_payment_value = self.calculate_coin_value(customer_payment)
-        if total_payment_value < selected_soda.price:
+        if total_payment_value > selected_soda.price:
             change_value = self.determine_change_value(total_payment_value, selected_soda.price)
             customer_change = self.gather_change_from_register(change_value)
             if customer_change is None:
-                self.user_interface.output_text('Dispensing ${total_payment_value} back to customer')
+                self.user_interface.output_text(f'Dispensing ${total_payment_value} back to customer')
                 customer.add_coins_to_wallet(customer_payment)
                 self.return_inventory(selected_soda)
             else:
