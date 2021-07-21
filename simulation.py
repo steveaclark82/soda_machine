42d03bb8912a8f8aede67309c22a43626efc3bf8
Steve Clark <steve.a.clark48@gmail.com>
2021-07-21 10:46:33 -0500

    Added imports from classes

--- simulation.py
+++ simulation.py
@@ -1,4 +1,6 @@
-
+from customer import Customer
+from soda_machine import SodaMachine
+from user_interface import UserInterface
 
 class Simulation:
     def __init__(self):
@@ -10,12 +12,12 @@ class Simulation:
         soda_machine = SodaMachine()
         will_proceed = False
         while will_proceed:
-            user_option = user_interface.simulation_main_menu()
-            if user_option = "1":
+            user_option = UserInterface.simulation_main_menu()
+            if user_option == "1":
                 soda_machine.begin_transaction(customer)
-            elif user_option = "2":
+            elif user_option == "2":
                 customer.check_coins_in_wallet()
-            elif user_option = "3":
+            elif user_option == "3":
                 customer.check_backpack()
             else:
                 will_proceed = False
